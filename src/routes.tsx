d453918e5a3e106859de06ab5728ea1cfa495b0f
Raphael Clemente Miranda <raphaelc484@hotmail.com>
2020-10-17 17:41:47 -0300

    teste

---
+++ src/routes.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+
+const {Navigator,Screen} = createStackNavigator();
+
+import OrphanagesMaps from './pages/OrphanagesMaps';
+import OrphanagesDetails from './pages/OrphanagesDetails'
+
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+
+import Header from './components/Header'
+
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+           <Navigator screenOptions=
+               {{
+                 headerShown:false,
+                 cardStyle:{
+                     backgroundColor:'#f2f3f5'
+                 }
+               }}>
+                <Screen 
+                    name="OrphanagesMaps" 
+                    component={OrphanagesMaps}
+                />
+
+                <Screen 
+                    name="OrphanagesDetails" 
+                    component={OrphanagesDetails}
+                    options={{
+                        headerShown:true,
+                        header: () => <Header showCancel={false} title='Orfanato' />
+                    }}
+                />
+
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown:true,
+                        header: () => <Header title='Selecione no mapa' />
+                    }}
+                />
+
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData}
+                    options={{
+                        headerShown:true,
+                        header: () => <Header title='Informeos os dados' />
+                    }}
+                />          
+
+           </Navigator> 
+        </NavigationContainer>
+    )
+}
